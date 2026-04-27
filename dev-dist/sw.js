b8fb81c5eaa9b44f20c199e8b9254ea13a127c41
Justice P. Belleza <justiceprimicias5@gmail.com>
2026-04-28 00:59:44 +0800

    Update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.aa78rfsq9e"
+    "revision": "0.kvmc9fafer"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
